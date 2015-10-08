e040bda5f410d6fa22f933441b2443cd7604dc10
kmaheshwari <maheshwari.kajol@gmail.com>
2015-10-08 18:20:20 +0530

    Templates created

--- cryptapp/urls.py
+++ cryptapp/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
    url(r'^$', views.main_page, name='main_page'),
+   url(r'^system_feature/', views.system_feature, name='system_feature'),
+   url(r'^about_us/', views.about_us, name='about_us'),
+   url(r'^system/', views.system, name='system'),
+   url(r'^issues/', views.issues, name='issues'),
+   url(r'^contact/', views.contact, name='contact'),
+   url(r'^feedback/', views.feedback, name='feedback'),
 ]
--- cryptapp/views.py
+++ cryptapp/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def main_page(request):
     return render(request, 'cryptapp/main_page.html', {})
+
+def system_feature(request):
+    return render(request, 'cryptapp/system_feature.html', {})
+
+def about_us(request):
+    return render(request, 'cryptapp/about_us.html', {})
+
+def system(request):
+    return render(request, 'cryptapp/system.html', {})
+
+def issues(request):
+    return render(request, 'cryptapp/issues.html', {})
+
+def contact(request):
+    return render(request, 'cryptapp/contact.html', {})
+
+def feedback(request):
+    return render(request, 'cryptapp/feedback.html', {})
